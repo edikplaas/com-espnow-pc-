227350c2c0fc4efaaadbbf9ed13de8e282f78eb1
edikplaas <edik.planson@laas.fr>
2025-05-27 11:27:56 +0200

    Ajout de la détection de la recharge de la batterie en USB

--- TX1/src/main.cpp
+++ TX1/src/main.cpp
@@ -3,8 +3,10 @@
 
 uint8_t broadcastAddress[] = {0x8C, 0xBF, 0xEA, 0xCC, 0xA8, 0xDC}; // Adresse MAC du récepteur
 unsigned long lastMicros = 0;
+unsigned long lastMillisCharge = 0;
 const unsigned long interval = 3333; // en µs, 2000µs correspond à 500Hz, 10000µs correspond à 100Hz
-
+const unsigned long intervalCharge = 10000; // en ms 
+bool modeRecharge = false;
 typedef struct struct_message {
   uint8_t entete1;
   uint8_t entete2;
@@ -21,6 +23,7 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 }
 
 void setup() {
+  Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   if (esp_now_init() != ESP_OK) {
     return;
@@ -38,15 +41,34 @@ void setup() {
 }
 
 void loop() {
+
+  float temperature = temperatureRead(); // Utilisation de la fonction native
+  Serial.println(temperature);
   unsigned long newTime = micros();
+  unsigned long newTimeCharge = millis();
   static uint8_t nb = 0;
-  if (newTime - lastMicros >= interval) {
-    lastMicros += interval;
+  if (newTime - lastMicros >= interval && !modeRecharge) {
+    lastMicros = newTime;
     for (int i = 0; i < 30; i++) {
       myData.bytes[i] = nb;
     }
     esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
     // result permet de vérifier le bon envoi des données
     nb++;
+  }
+
+  if (newTimeCharge-lastMillisCharge>=intervalCharge){
+    lastMillisCharge = newTimeCharge;
+    uint32_t VCC_value = 0;
+    for(int i = 0; i < 16; i++) {
+      VCC_value += analogReadMilliVolts(A0); // Read and accumulate ADC voltage
+    }
+    float VCC_valueF = 2 * VCC_value / 16 / 1000.0;     // Adjust for 1:2 divider and convert to volts
+    if (VCC_valueF>2.5){
+      modeRecharge = true;
+    }
+    else{
+      modeRecharge = false;
+    }
   }
 }
